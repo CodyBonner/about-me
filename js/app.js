27a9ea17a30eb4ebc6f456e265f079bf16c6c541
Cody Bonner <bonner_cody94@yahoo.com>
2023-08-05 17:03:54 +0000

    added conditional function for one question

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+console.log("hello world!");
+
+let userName = prompt("What is your name?");
+let a1 = userName;
+
+console.log("Username is: " + a1);
+
+let fishing = prompt("Does Thomas like fishing?").toLowerCase();
+
+
+
+//conditionals
+
+if(fishing === "y" || fishing === "yes"){
+
+  alert("That's correct. Thomas loves fishing");
+}
+else{
+  alert("That's wrong! He does in fact love fishing");
+
+}
+
+
+
+//end goal
+
+
