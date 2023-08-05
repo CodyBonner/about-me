f0c6dffeaf4c98bac94c9d9ad31aacc67f2a3c66
Cody Bonner <bonner_cody94@yahoo.com>
2023-08-05 20:54:55 +0000

    fixed errors

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-console.log("hello world!");
+//console.log("hello world!");
 
 alert("Welcome to the site! Hope you enjoy your stay. In the following prompt, I Cody the software developer will ask for your name as well as some follow up questions. Good luck and have fun!");
 
@@ -9,7 +9,7 @@ alert("Welcome to the site! Hope you enjoy your stay. In the following prompt, I
 let userName = prompt("What is your name?");
 let a1 = userName;
 alert("Welcome " + a1 + "." + " Let's see how much you know about Cody!");
-console.log("Username is: " + a1);
+//console.log("Username is: " + a1);
 
 //Question One
 
@@ -20,11 +20,11 @@ let q1 = prompt("Does Cody like playing Video Games?").toLowerCase();
 if(q1 === "y" || q1 === "yes"){
 
   alert("That's correct. Cody loves to play video games. So much so that he even streams them!");
-  console.log("Bingo!");
+  //console.log("Bingo!");
 }
 else{
   alert("That's wrong! He does in fact love playing video games");
-  console.log("Wrong, he loves them");
+  //console.log("Wrong, he loves them");
 
 }
 
@@ -36,11 +36,11 @@ let q2 = prompt("Is Cody the youngest grandchild?").toLowerCase();
 
 if(q2 === "y" || q2 ==="yes"){
   alert("That's incorrect, Cody is actually the oldest of the grandchildren in his family");
-  console.log("Nope, cody is the oldest");
+  //console.log("Nope, cody is the oldest");
 }
 else{
   alert("Right you are! Cody is actually the oldrest grandchild of his family, clocking in at 28 years old!");
-  console.log("Correct");
+  //console.log("Correct");
 }
 
 //question three
@@ -51,11 +51,11 @@ let q3 = prompt("Does Cody have any siblings?").toLowerCase();
 
 if(q3 === "n"|| q3 ==="no"){
   alert("Not true! Cody actually does have siblings!");
-  console.log("Nope that's wrong");
+  //console.log("Nope that's wrong");
 }
 else{
   alert("Winner Winner chicken dinner you're right! Cody does in fact have one sibling a younger brother");
-  console.log("Yes you're right!");
+  //console.log("Yes you're right!");
 }
 
 //question four
@@ -66,11 +66,11 @@ let q4 = prompt("Has Cody ever gone to college?").toLowerCase();
 
 if(q4 === "yes" || q4 === "y"){
   alert("That's right! Cody has been to college, he's even graduated too!");
-  console.log("That's right cody has been to college!");
+  //console.log("That's right cody has been to college!");
 }
 else{
   alert("Sorry my dear crusader, but you are incorrect. Cody has indeed been to college.");
-  console.log("That's not right!");
+  //console.log("That's not right!");
 }
 
 
@@ -81,12 +81,12 @@ let q5 = prompt("Do you want to know more about Cody?");
 if(q5 === "yes" || q5 === "y"){
   alert("Welcome to the site then! Here you'll learn just a little bit more about the software developer Cody!");
   alert("Enjoy your stay! And if needed ask questions.");
-  console.log("Awesome!");
+  //console.log("Awesome!");
 }
 
 else{
   alert("Oh...Well that's okay! Hope you enjoyed your time here anyways!");
-  console.log("Rats");
+// console.log("Rats");
 }
 
 alert("Once again as stated in the beginning, welcome to the page and enjoy your stay " + a1);
